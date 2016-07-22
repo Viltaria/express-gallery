a1b967999bbf5537c6070444a82991800d8b7ad3
Tyler Chong <tylrchong@gmail.com>
2016-07-22 11:39:39 -1000

    Footer + Verification

--- server.js
+++ server.js
@@ -21,13 +21,38 @@ app.use(methodOverride(function(req, res){
 app.set('view engine', 'jade');
 app.set('views', './templates');
 
+function verification (req, res, next) {
+  var body = req.body;
+  var supportedFileTypes = ['.tif','.jpg','.png','.jpeg'],
+      fileType;
+  if(req.method === 'POST') {
+      fileType = body.link.slice(body.link.lastIndexOf('.'), body.link.length);
+    if(!body.author || !body.link || !body.description) {
+      //return render error page
+    } else if(supportedFileTypes.indexOf(fileType) < 0) {
+      // return render error page
+    }
+  } else if (req.method === 'PUT') {
+      fileType = body.link.slice(body.link.lastIndexOf('.'), body.link.length);
+      if(supportedFileTypes.indexOf(fileType) < 0) {
+        //return render error page
+      }
+      for(var key in body) {
+        if(!body[key]) {
+          //return render error page
+        }
+      }
+  }
+  next();
+}
+
 var PORTNUM = 3000;
 
 var db = require('./models'),
     Gallery = db.Gallery;
 
 var rootPaths = ['/', '/gallery', '/galler', '/galle', '/gall', '/gal', '/ga', '/g'];
-app.get(rootPaths, (req, res) => {
+app.get(rootPaths, verification, (req, res) => {
   sequelize.query('SELECT * FROM "Galleries"', {type: sequelize.QueryTypes.SELECT})
   .then ( (data) => {
     res.render('gallery/index',{
@@ -37,10 +62,10 @@ app.get(rootPaths, (req, res) => {
     //res.render error page
   });
 });
-app.get('/gallery/new', (req, res) => {
+app.get('/gallery/new', verification, (req, res) => {
   res.render('gallery/new');
 });
-app.get('/gallery/:id', (req, res) => {
+app.get('/gallery/:id', verification, (req, res) => {
   sequelize.query(`SELECT * FROM "Galleries" WHERE id = ${req.params.id}`, {type: sequelize.QueryTypes.SELECT})
   .then( (data) => {
     res.render('gallery/index',{
@@ -50,7 +75,7 @@ app.get('/gallery/:id', (req, res) => {
     //res render error page
   });
 });
-app.post('/gallery', (req, res) => {
+app.post('/gallery', verification, (req, res) => {
   Gallery.create({
     author: req.body.author,
     link: req.body.link, //encodeURL?
@@ -61,7 +86,7 @@ app.post('/gallery', (req, res) => {
     //res render error page
   });
 });
-app.get('/gallery/:id/edit', (req, res) => {
+app.get('/gallery/:id/edit', verification, (req, res) => {
   sequelize.query(`SELECT * FROM "Galleries" WHERE id = ${req.params.id}`, {type: sequelize.QueryTypes.SELECT})
   .then( (data) => {
     res.render('gallery/edit', {
@@ -72,10 +97,7 @@ app.get('/gallery/:id/edit', (req, res) => {
     //res render error page
   });
 });
-app.put('/gallery/:id', (req, res) => {
-  // for(var key in req.body) {
-  //   sequelize.query(`UPDATE "Galleries" SET ${key} = ${req.body[key]} WHERE id = ${req.params.id}`, {type: sequelize.QueryTypes.UPDATE});
-  // }
+app.put('/gallery/:id', verification, (req, res) => {
   Gallery.update(
     {
       author: req.body.author,
@@ -97,8 +119,13 @@ app.put('/gallery/:id', (req, res) => {
     //res.render Error page
   });
 });
-app.delete('/gallery/:id', (req, res) => {
-  sequelize.query(`DELETE FROM "Galleries" WHERE id = ${req.params.id}`, {type: sequelize.QueryTypes.DELETE});
+app.delete('/gallery/:id', verification, (req, res) => {
+  sequelize.query(`DELETE FROM "Galleries" WHERE id = ${req.params.id}`, {type: sequelize.QueryTypes.DELETE})
+  .then( (data) => {
+    if(data.rowCount === 0) {
+      //return render error page
+    }
+  });
   sequelize.query('SELECT * FROM "Galleries"', {type: sequelize.QueryTypes.SELECT})
   .then ( (data) => {
     res.render('gallery/index',{
@@ -108,7 +135,7 @@ app.delete('/gallery/:id', (req, res) => {
     //res.render Error page
   });
 });
-app.get('*', (req, res) => {
+app.get('*', verification, (req, res) => {
   //res.render 404 page
 });
 
