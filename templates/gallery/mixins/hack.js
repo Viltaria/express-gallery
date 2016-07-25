f03ec8d21f94f45389f8d9f30a444feda5ff20a7
Tyler Chong <tylrchong@gmail.com>
2016-07-25 13:29:15 -1000

    Did page numbers next/prev buttons

--- templates/gallery/mixins/hack.js
+++ templates/gallery/mixins/hack.js
@@ -6,6 +6,13 @@ x = Number(x.slice(x.lastIndexOf('/') + 1, x.length));
 if(location.pathname === '/gallery') {
   x = 1;
 }
+if(x <= 1) {
+  prev.style.display = "none";
+}
+var numImages = document.getElementsByTagName('img');
+if(numImages.length !== 21) {
+  next.style.display = "none";
+} //leaves edge case of where 21 pictures on one page, but none on next one
 if(prev && next) {
   prev.addEventListener('click', () => {
     location.assign('/gallery/page/' + (x -1));
