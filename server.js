7227e09e4c0a3162a05d7ee829f8b27addd66d97
Tyler Chong <tylrchong@gmail.com>
2016-07-27 14:39:42 -1000

    pg

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ var session = require('express-session');
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt');
 var flash = require('connect-flash');
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.set('view engine', 'jade');
 
